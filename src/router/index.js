335393883cda9f848a9c5d7e4639da115dfa94be
7code <7code@mxt-c.com>
2022-10-03 16:25:23 +0200

    AddPermit page added. Router naming bug fixed

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import PlateListView from "../views/PlateListView";
+import PermitListView from "../views/PermitListView";
 
 Vue.use(VueRouter);
 
@@ -8,7 +8,7 @@ const routes = [
   {
     path: "/",
     name: "permitList",
-    component: PlateListView
+    component: PermitListView
   },
   {
     path: "/addPermit",
@@ -16,7 +16,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/addPermit.vue")
+    component: () => import(/* webpackChunkName: "about" */ "../views/AddPermitView.vue")
   }
 ];
 
--- src/views/AddPermitView.vue
+++ src/views/AddPermitView.vue
@@ -1,3 +1,71 @@
 <template>
-  <h1>This is an add plate page</h1>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="6">
+        <h2 class="text-center mb-2 mt-2">Add new permit 📄</h2>
+        <v-form v-model="valid">
+          <v-container>
+            <v-row>
+              <v-col cols="6">
+                <v-autocomplete
+                  :items="plateIssuerCountry"
+                  item-text="label"
+                  item-value="code"
+                  label="Country"
+                ></v-autocomplete>
+              </v-col>
+
+              <v-col cols="6">
+                <v-text-field v-model="licencePlate" label="License plate" required></v-text-field>
+              </v-col>
+
+              <v-col cols="6"> DataPicker component </v-col>
+
+              <v-col cols="6"> DataPicker component </v-col>
+
+              <v-col cols="12">
+                <v-text-field v-model="ownerName" label="Owner name"></v-text-field>
+              </v-col>
+
+              <v-col class="text-center">
+                <v-btn class="mr-5">Clear Form</v-btn>
+                <v-btn color="success">Add Permit</v-btn>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+const COUNTRIES = [
+  {
+    code: "DE",
+    label: "Germany"
+  },
+  {
+    code: "F",
+    label: "France"
+  },
+  {
+    code: "CH",
+    label: "Switzerland"
+  },
+  {
+    code: "AT",
+    label: "Austria"
+  }
+];
+
+export default {
+  name: "AddPermitView",
+  data: () => ({}),
+  computed: {
+    plateIssuerCountry() {
+      return COUNTRIES;
+    }
+  }
+};
+</script>
