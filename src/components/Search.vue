21319aff01e40da596fecb37916d35fd542a93a9
7code <7code@mxt-c.com>
2022-10-08 23:04:01 +0200

    Search component added

---
+++ src/components/Search.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="10">
+          <v-text-field v-model="query" label="Search" required></v-text-field>
+        </v-col>
+        <v-col cols="2">
+          <v-btn class="mt-3" @click="onSearch" color="primary"
+            ><v-icon left> mdi-magnify</v-icon> Search</v-btn
+          >
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: "SearchComponent",
+  props: {
+    store: {
+      type: String,
+      required: true
+    }
+  },
+  data: () => ({
+    query: ""
+  }),
+  methods: {
+    onSearch() {
+      if (!this.query.length) return;
+    }
+  }
+};
+</script>
